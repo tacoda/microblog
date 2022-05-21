ba33b104e237bf4828058250aa6c61b302fc266f
Ian Johnson <ijohnson@covermymeds.com>
2022-05-20 21:44:37 -0400

    Update main script

--- myapp.py
+++ myapp.py
@@ -4,12 +4,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+@app.route('/')
+@app.route('/index')
+def index():
     return "Hello, Nix!"
 
 def run():
-    app.run(host="0.0.0.0", port=5001)
+    app.run()
 
 if __name__ == "__main__":
     run()
