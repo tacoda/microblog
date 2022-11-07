88f441ec0d3ef96015c39e61f2fb402d92369f74
Ian Johnson <ijohnson@covermymeds.com>
2022-11-07 18:15:10 -0500

    Update port

--- verify.sh
+++ verify.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 python myapp.py >> /dev/null 2>&1 &
 pid=$!
 
-if [[ $(curl --retry 3 --retry-delay 1 --retry-connrefused http://127.0.0.1:5001) == "Hello, Nix!" ]]; then
+if [[ $(curl --retry 3 --retry-delay 1 --retry-connrefused http://127.0.0.1:5000) == "Hello, Nix!" ]]; then
     echo "SUCCESS: myapp.py is serving the expected string"
     kill $pid
     exit 0
