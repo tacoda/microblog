c96e93b074a2a80755814c5f50847c0e72631360
Ian Johnson <ijohnson@covermymeds.com>
2022-11-17 16:36:17 -0500

    Add structure

--- myapp.py
+++ myapp.py
@@ -1,13 +1,4 @@
-#! /usr/bin/env python
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, Nix!"
+from app import app
 
 def run():
     app.run()
